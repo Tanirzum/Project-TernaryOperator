4efd5dd221f979e932b4ce310c9d70b4542e8fd5
Tanirbergen <87113165+Tanirzum@users.noreply.github.com>
2021-10-25 00:02:36 +0600

    FinallCommit

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
+        int a = -2;
+        System.out.println(a >= 0 ? "Good" : "Error");
+
+//        if (a >= 0) {
+//            System.out.println("Good");
+//        } else {
+//            System.out.println("Error");
+//        }
+    }
+}
